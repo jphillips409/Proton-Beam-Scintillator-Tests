910aa8118a9db6bfe5253b277ec00bad86c894a8
jphillips409 <138801733+jphillips409@users.noreply.github.com>
2026-02-23 17:03:40 -0600

    Add files via upload

--- Unpack.py
+++ Unpack.py
@@ -19,6 +19,7 @@ from scipy.optimize import minimize
 from scipy.integrate import quad
 import matplotlib as mpl
 from pathlib import Path
+import math
 
 # Sets the plot style
 plt.rcParams['font.size'] = 12
@@ -48,19 +49,22 @@ plt.rcParams['ytick.minor.visible'] = True
 plt.rcParams['font.size'] = 15
 plt.rcParams['axes.titlepad'] = 15
 
+figurepath = Path('OutputFigures') # Go outside of directory and enter Paper_Figures
+figurepath = '..' / figurepath
 
 # Unpacks the CAEN data in csv format. Can choose to unpack all data or some using alldat
 # Can also choose to import the waveforms (a lot of data) or just the first 6 columns
 #   which contain the important information for each event
+# Flag parameter (index 5) must be converted from Hex to 32int
 def Unpack(name, alldat, waves):
     if alldat is True and waves is True:
-        data = np.genfromtxt(name, skip_header = True, delimiter = ';')
+        data = np.genfromtxt(name, skip_header = True, delimiter = ';',converters={5: lambda s: float.fromhex(s)})
     elif alldat is False and waves is True:
-        data = np.genfromtxt(name, skip_header = True, delimiter =';', max_rows = 50000)
+        data = np.genfromtxt(name, skip_header = True, delimiter =';', max_rows = 50000,converters={5: lambda s: float.fromhex(s)})
     elif alldat is True and waves is False:
-        data = np.genfromtxt(name, skip_header = True, delimiter =';', usecols = (0, 1, 2, 3, 4, 5))
+        data = np.genfromtxt(name, skip_header = True, delimiter =';', usecols = (0, 1, 2, 3, 4, 5),converters={5: lambda s: float.fromhex(s)})
     else:
-        data = np.genfromtxt(name, skip_header = True, delimiter =';', max_rows = 10000, usecols = (0, 1, 2, 3, 4, 5))
+        data = np.genfromtxt(name, skip_header = True, delimiter =';', max_rows = 10000, usecols = (0, 1, 2, 3, 4, 5),converters={5: lambda s: float.fromhex(s)})
 
     return data
 
@@ -108,7 +112,7 @@ def EDisc(energies,energies_short,times,channels,traces,elow,ehigh):
 # Also determine filepath here
 def GetFile():
     # file path, use 'Path' function to make compatible with windows/mac
-    filepath = Path(r"/Users/jphillips409/Documents/SobotkaLab/ProtonBeam_ScintTests/Data/Feb14_2026")
+    filepath = Path(r"../Data/Feb14_2026")
 
     # Data files
 
@@ -116,13 +120,28 @@ def GetFile():
 
     # Feb 14, 2026
     # 10 pulses
-    #file = r'ProtonBeam_Feb14_2026_10pulses_a/RAW/SDataR_ProtonBeam_Feb14_2026_10pulses_a.csv'
+    file = r'ProtonBeam_Feb14_2026_10pulses_a/RAW/SDataR_ProtonBeam_Feb14_2026_10pulses_a.csv'
     #file = r'ProtonBeam_Feb14_2026_10pulses_b/RAW/SDataR_ProtonBeam_Feb14_2026_10pulses_b.csv'
-    file = r'ProtonBeam_Feb14_2026_10pulses_c/RAW/SDataR_ProtonBeam_Feb14_2026_10pulses_c.csv'
+    #file = r'ProtonBeam_Feb14_2026_10pulses_c/RAW/SDataR_ProtonBeam_Feb14_2026_10pulses_c.csv'
+
+    # 20 pulses
+    #file = r'ProtonBeam_Feb14_2026_20pulses_a/RAW/SDataR_ProtonBeam_Feb14_2026_20pulses_a.csv'
+    #file = r'ProtonBeam_Feb14_2026_20pulses_b/RAW/SDataR_ProtonBeam_Feb14_2026_20pulses_b.csv'
+    #file = r'ProtonBeam_Feb14_2026_20pulses_c/RAW/SDataR_ProtonBeam_Feb14_2026_20pulses_c.csv'
+
+    # 100 pulses
+    #file = r'ProtonBeam_Feb14_2026_100pulses_a/RAW/SDataR_ProtonBeam_Feb14_2026_100pulses_a.csv'
+    #file = r'ProtonBeam_Feb14_2026_100pulses_b/RAW/SDataR_ProtonBeam_Feb14_2026_100pulses_b.csv'
+    #file = r'ProtonBeam_Feb14_2026_100pulses_c/RAW/SDataR_ProtonBeam_Feb14_2026_100pulses_c.csv'
+
+    # 10 pulses - no scintillator
+    #file = r'ProtonBeam_Feb14_2026_BareFib_a/RAW/SDataR_ProtonBeam_Feb14_2026_BareFib_a.csv'
+    #file = r'ProtonBeam_Feb14_2026_BareFib_b/RAW/SDataR_ProtonBeam_Feb14_2026_BareFib_b.csv'
+    #file = r'ProtonBeam_Feb14_2026_BareFib_c/RAW/SDataR_ProtonBeam_Feb14_2026_BareFib_c.csv'
 
     ###################################################
 
-    return filepath / file
+    return filepath / file, file
 
 def main():
     # Read the CAEN data file in csv format
@@ -136,8 +155,25 @@ def main():
     # The Path library will use forward slashes but convert them to correctly treat your OS
     # Also makes it easier to switch to a different computer
 
-    datafile = GetFile()
+    datafile, filecsv = GetFile()
     print("Analyzing file: ", datafile)
+
+    # Reads the data file string and gets some of the name
+    # Useful for saving figures
+    data_string = list(filecsv)
+    savename = []
+    for i in range(len(data_string)):
+        # First search for RAW
+        if i != 0 and data_string[i] == 'W' and data_string[i-1] == 'A' and data_string[i-2] == 'R':
+            for j in range(i + 1, len(data_string)):
+                if data_string[j] == '_' and data_string[j-1] == 'm' and data_string[j-2] == 'a' and data_string[j-3] == 'e':
+                    for k in range(j + 1, len(data_string)):
+                        if data_string[k] == '.': break
+                        savename.append(data_string[k])
+                else: continue
+        else: continue
+    savename = ''.join(savename)
+    print('Figure save name: ', savename)
 
     alldat = True
     wavesdat = True
@@ -152,6 +188,7 @@ def main():
     time = []
     channel = []
     traces = []
+    flags = []
 
     satevents = 0 #  Counts the number of saturated events
 
@@ -161,8 +198,9 @@ def main():
         channel.append(data[i][1])
         time.append(data[i][2])
         energy.append(data[i][3])
-        if data[i][3] == 4095: satevents +=1
+        if data[i][5] == 16512: satevents +=1
         energy_short.append(data[i][4])
+        flags.append(data[i][5])
         if wavesdat is True: traces.append(data[i][6:])
 
     print("Data unpacked")
@@ -174,6 +212,7 @@ def main():
     time = np.array(time)
     channel = np.array(channel)
     traces = np.array(traces)
+    flags = np.array(flags)
 
     # Get energies discriminated on elow and ehigh
     # Will return all arrays using Ecuts
@@ -183,6 +222,8 @@ def main():
     # Specify a number of bins
     nbins = 512
 
+    print('Number of energies above/below thresholds: ', len(energy_disc))
+
     # Plots the raw spectrum
     fig_rawE, ax_rawE = plt.subplots()
     ax_rawE.hist(energy, bins=nbins, range=[0, 4095])
@@ -191,12 +232,20 @@ def main():
     ax_rawE.set_ylabel('Counts')
     plt.show()
 
+    for i in range(len(channel)):
+        if flags[i] == 16512: print('Saturated event index: ', i)
+
     # Plot of energies vs index (raw)
     plot_EvsT, ax_EvsT = plt.subplots()
     xspace = np.linspace(0, len(channel),len(channel))
     ax_EvsT.scatter(xspace, energy)
     ax_EvsT.set_ylabel('ADC Channel')
     ax_EvsT.set_xlabel('index')
+    figgrid_name = savename + '_EvsID'
+    plt.savefig(figurepath / (figgrid_name + '.eps'), format='eps')
+    plt.savefig(figurepath / (figgrid_name + '.png'), format='png')
+    plt.savefig(figurepath / (figgrid_name + '.svg'), format='svg')
+    plt.show()
     plt.show()
 
     # Plot of energies vs Time (discriminated)
@@ -205,6 +254,13 @@ def main():
     ax_dEvsT.scatter(time_disc/(10**12), energy_disc)
     ax_dEvsT.set_ylabel('ADC Channel')
     ax_dEvsT.set_xlabel('Time (s)')
+    #ax_dEvsT.set_ylim(900,1500)
+    ax_dEvsT.set_ylim(0,600) # Without scintillator
+
+    figgrid_name = savename + '_discEvsT'
+    plt.savefig(figurepath / (figgrid_name + '.eps'), format='eps')
+    plt.savefig(figurepath / (figgrid_name + '.png'), format='png')
+    plt.savefig(figurepath / (figgrid_name + '.svg'), format='svg')
     plt.show()
 
     # Plot traces
@@ -214,30 +270,62 @@ def main():
     # Convert sample number to time
     # CAEN samples every 2 ns
     wavetime = wavetime * 2
-    plot_traces, ax_traces = plt.subplots()
-    ax_traces.plot(wavetime,traces[4])
+
+    # Plot raw traces 1 at a time
+    plot_dtraces, ax_dtraces = plt.subplots()
+    ax_dtraces.plot(wavetime,traces[11])
     #plt.xlim(500,1500)
     plt.show()
 
     # Plot discriminated traces 1 at a time
     plot_dtraces, ax_dtraces = plt.subplots()
-    ax_dtraces.plot(wavetime,traces_disc[0])
+    ax_dtraces.plot(wavetime,traces_disc[11])
     #plt.xlim(500,1500)
     plt.show()
 
     # make a window plot showing each real traces
-    fig_multtrac, ax_multtrac = plt.subplots(5,5)
-    row_num = 0
-    col_num = 0
-    for row in ax_multtrac:
-        for col in row:
-            if row_num + col_num == len(traces_disc): break
-            col.plot(wavetime, traces_disc[row_num + col_num])
-            col_num+=1
+    # Find number of windows needed using 5 columns
+    numcols = 5
+    if len(traces_disc) > 50: numcols = 10
+    numrows = math.ceil(len(traces_disc)/numcols)
 
-        if row_num + col_num == len(traces_disc): break
-        row_num+=1
+    windowsize = (20,20)
+    if len(traces_disc) > 50: windowsize = (40,40)
 
+    fig_multtrac, ax_multtrac = plt.subplots(numrows,numcols,sharex=True,sharey=True, figsize=windowsize,layout='compressed')
+    axes_flat = ax_multtrac.flatten()
+
+    for i, ax in enumerate(axes_flat):
+        if i < len(traces_disc):
+            pulselabel = 'Pulse ' + str(i+1)
+            ax.set_box_aspect(1)
+            ax.plot(wavetime, traces_disc[i])
+            ax.text(0.95,0.05,pulselabel,transform=ax.transAxes,ha='right',va='bottom',)
+            #ax.legend(loc='lower right')
+
+            axes_flat[i].label_outer()
+        else:
+            axes_flat[i].remove()
+    # Manually re-enable labels for plots that have nothing below them
+    for i in range(len(traces_disc)):
+        # If the index plus one column width is outside the plot count, it's a bottom plot
+        if i + 5 >= len(traces_disc):
+            axes_flat[i].tick_params(labelbottom=True)
+    fig_multtrac.canvas.draw()
+    xaxis_shift = 0
+    last_ax = axes_flat[len(traces_disc) -1]
+    renderer = fig_multtrac.canvas.get_renderer()
+    bbox = last_ax.get_tightbbox(renderer)
+    xaxis_shift = fig_multtrac.transFigure.inverted().transform((0, bbox.y0))[1] - 0.02
+
+    fig_multtrac.supxlabel('Time (ns)',fontsize=30,y=xaxis_shift)
+    fig_multtrac.supylabel('ADC Channels',fontsize=30)
+    #plt.subplots_adjust(wspace=0.1, hspace=0.1)
+
+    figgrid_name = savename + '_wavegrid'
+    plt.savefig(figurepath / (figgrid_name + '.eps'), format='eps')
+    plt.savefig(figurepath / (figgrid_name + '.png'), format='png')
+    plt.savefig(figurepath / (figgrid_name + '.svg'), format='svg')
     plt.show()
 
 
